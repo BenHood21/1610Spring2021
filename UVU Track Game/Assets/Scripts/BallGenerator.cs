0cfabf6787204f66be5790f23a4a8d25b7856af0
Benjamin Hood <10885066@my.uvu.edu>
2021-01-29 11:27:11 -0700

    Update In Unity Files
    Added Ball Generator and Practice Scripts

---
+++ UVU Track Game/Assets/Scripts/BallGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BallGenerator : MonoBehaviour
+{
+    private int number = 0;
+    public GameObject ball;
+
+    IEnumerator Start()
+    {
+        while (number < 25)
+        {
+            yield return new WaitForSeconds(0.5f);
+            Instantiate(ball);
+            number++;
+        }
+
+    
+    }
+}    
