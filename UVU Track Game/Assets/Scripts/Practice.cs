6a354035209c4fb941c70b115ea0eb2c9215a834
Benjamin Hood <10885066@my.uvu.edu>
2021-01-29 12:32:54 -0700

    Added Scope and Access Modifiers Practice

--- UVU Track Game/Assets/Scripts/Practice.cs
+++ UVU Track Game/Assets/Scripts/Practice.cs
@@ -12,7 +12,7 @@ public class Practice : MonoBehaviour
 		}
 		if (Input.GetKeyDown(KeyCode.G))
 		{
-		GetComponent<Renderer>().material.color = Color.green;
+			GetComponent<Renderer>().material.color = Color.green;
 		}
 		if (Input.GetKeyDown(KeyCode.B))
 		{
@@ -20,4 +20,57 @@ public class Practice : MonoBehaviour
 		}
 		
 	}
+	
+		public class ScopeAndAccessModifiers : MonoBehaviour
+		{
+			public int alpha = 5;
+			
+			private int beta = 0;
+			private int gamma = 5;
+			
+			private AnotherClass myOtherClass;
+
+			void Start ()
+			{
+				alpha = 29;
+
+				myOtherClass = new AnotherClass();
+				myOtherClass.FruitMachine(alpha, myOtherClass.apples);
+			}
+			
+			void Example (int pens, int crayons)
+			{
+				int answer;
+				answer = pens * crayons * alpha;
+				Debug.Log(answer);
+			}
+				
+			void Update ()
+			{
+				Debug.Log("Alpha is set to: " + alpha);
+			}
+		}
+		
+		public class AnotherClass
+		{
+			public int apples;
+			public int bananas;
+			
+			private int stapler;
+			private int sellotape;
+			
+			public void FruitMachine (int a, int b)
+			{
+				int answer;
+				answer = a + b;
+				Debug.Log ("fruit total: " + answer);
+			}
+			
+			private void OfficeSort (int a, int b)
+			{
+				int answer;
+				answer = a + b;
+				Debug.Log("Office Supplies total: " + answer);
+			}
+		}
 }
