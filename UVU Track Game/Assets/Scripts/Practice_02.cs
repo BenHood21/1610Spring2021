163eeaeecbc9cc85bb02d3a70a2c21329eef2906
Benjamin Hood <10885066@my.uvu.edu>
2021-02-06 13:57:44 -0700

    Added practice Line

---
+++ UVU Track Game/Assets/Scripts/Practice_02.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Practice_02 : MonoBehaviour
+{
+    public class EnableComponents : MonoBehaviour
+    {
+        private Light myLight;
+
+            void Start()
+            {
+                myLight = GetComponent<Light>();
+            }
+
+        void Update()
+        {
+            if(Input.GetKeyUp(KeyCode.Space))
+            {
+                myLight.enabled = !myLight.enabled;
+            }
+        }
+    }
+}
