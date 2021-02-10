0447837e836ff92d9fa47208ecdaa3fe8c5f7d22
Benjamin Hood <10885066@my.uvu.edu>
2021-02-10 11:37:58 -0700

    Added update lines to UserInput, counter, and Intdata

--- UVU Track Game/Assets/Scripts/userInput.cs
+++ UVU Track Game/Assets/Scripts/userInput.cs
@@ -4,27 +4,19 @@ using UnityEngine;
 
 public class userInput : MonoBehaviour
 {
-	public Rigidbody2D ballRigidbody2D;
 	public float speed = 3f;
-	public float jumpForce = 15f;
-
-
-
+	public float jumpForce = 20f;
+	public Rigidbody2D rigidbodyObj;
 	private Vector2 direction;
-	private Vector2 yDirection;
 
-	private int score;
-	
 	private void Update()
 	{
-		direction.x = Input.GetAxis("Horizontal");
-		if (Input.GetButtonDown("Jump")) return;
-		yDirection.y = jumpForce;
+		direction.x = speed * Input.GetAxis("Horizontal");
+		rigidbodyObj.AddForce(direction, ForceMode2D.Force);
+		if (Input.GetButtonDown("Jump"))
+		{ 
+			direction.y = jumpForce;
+			rigidbodyObj.AddForce(direction, ForceMode2D.Impulse);
+		}
 	}
-	private void FixedUpdate()
-	{
-		var newDirection = direction * (speed * Time.deltaTime);
-		ballRigidbody2D.AddForce(direction, ForceMode2D.Force);
-		ballRigidbody2D.AddForce(yDirection, ForceMode2D.Force);
-	}
-}	
+}
