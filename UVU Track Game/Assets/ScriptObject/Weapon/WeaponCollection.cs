cd3e15f6cca8948be07c8b805d79287b4702c66b
Ben Hood <10885066@my.uvu.edu>
2021-02-26 15:55:14 -0700

    Added daily work for weapons update

---
+++ UVU Track Game/Assets/ScriptObject/Weapon/WeaponCollection.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class WeaponCollection : ScriptableObject
+{
+   public List<Weapon> weaponsList;
+}
---
+++ UVU Track Game/Assets/Scripts/Behavior/WeaponCollectionBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WeaponCollectionBehavior : MonoBehaviour
+{
+    public Weapon weaponObj;
+    public WeaponCollection weaponCollectionObj;
+
+    private void Start()
+    {
+        foreach (var weapon in weaponCollectionObj.weaponsList)
+        {
+            if (weapon == weaponObj)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        weaponCollectionObj.weaponsList.Add(weaponObj);
+        gameObject.SetActive(false);
+    }
+}
