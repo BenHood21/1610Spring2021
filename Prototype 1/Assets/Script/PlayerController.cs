4a5ffcba138af5c4ba0170db2ae6d20f1df0531d
Ben Hood <10885066@my.uvu.edu>
2021-03-01 17:11:11 -0700

    Created 3D car model, followed Unity tutorial, going to mess around with more scripting

---
+++ Prototype 1/Assets/Script/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float vehicleSpeed = 10.0f;
+    private  float turnSpeed = 40f;
+    private float horizontalInput;
+    private float forwardInput;
+    
+    // Forward/reverse movement control and turn control
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        
+        transform.Translate(Vector3.forward * Time.deltaTime * vehicleSpeed * forwardInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
