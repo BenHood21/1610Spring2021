c1289b28d080b84db7da394e6321bd53e964bb05
Ben Hood <10885066@my.uvu.edu>
2021-03-02 22:45:12 -0700

    I think I fixed the problems in the Plane Challenge

---
+++ Prototype 1/Assets/Challenge 1/Scripts/PropellerSpin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerSpin : MonoBehaviour
+{
+    public float rotationSpeed;
+    
+    // Start is called before the first frame update
+    void Update()
+    {
+        transform.Rotate(new Vector3(0,0, Time.deltaTime * rotationSpeed));
+    }
+}
