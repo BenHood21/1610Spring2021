00a19eea45958b3d8d46d24f543224c7d459af06
Ben Hood <10885066@my.uvu.edu>
2021-03-05 12:03:57 -0700

    Meteors made to fall from the sky

---
+++ Prototype 1/Assets/Challenge 1/Scripts/RockDropBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockDropBehaviour : MonoBehaviour
+{
+    public GameObject target;
+
+    private Vector3 movement;
+    public float gravity;
+    void Start()
+    {
+        Invoke ("DropRock", 2);
+    }
+    
+    void DropRock()
+    {
+        float x = Random.Range(-8.0f, 8.0f);
+        float z = Random.Range(-10.0f, 190.0f);
+        Instantiate(target, new Vector3(x, 20, z), Quaternion.identity);
+        movement.y = gravity * Time.deltaTime; 
+    }
+}
