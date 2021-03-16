1795e5a1c9bbb005aa7b05f63f7c706a0eb98558
Ben Hood <10885066@my.uvu.edu>
2021-03-16 17:56:08 -0600

    Food and animal movement and prefab edits

--- Prototype 2/Prototype 2/Assets/Scrpits/PlayerControllerBehaviour.cs
+++ Prototype 2/Prototype 2/Assets/Scrpits/PlayerControllerBehaviour.cs
@@ -10,19 +10,26 @@ public class PlayerControllerBehaviour : MonoBehaviour
     public float horizontalInput;
     public float speed = 50;
     public float xRange = 20;
-
+    public GameObject projectilePrefab, projectilePrefab2;
+    
     private Vector3 direction = Vector3.zero;
-    public bool gameOver = false;
+    
     
 
     private void Update()
     {
-
+        // Movement
         horizontalInput = Input.GetAxis("Horizontal");
         direction.Set(horizontalInput * speed, 0, 0);
         transform.Translate(direction * Time.deltaTime);
 
+        // Projectile key comand
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
 
+        // Movement Boundary
         if (transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
