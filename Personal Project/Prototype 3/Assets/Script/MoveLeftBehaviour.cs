c63c38a59794658b02b1d2defeedb4de95164522
Ben Hood <10885066@my.uvu.edu>
2021-03-22 19:22:47 -0600

    Started Prototype 3 and finished Jump Force Challenge (Unversioned Files)

---
+++ Personal Project/Prototype 3/Assets/Script/MoveLeftBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeftBehaviour : MonoBehaviour
+{
+    private float speed = 10;
+
+    private void Update()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
+    }
+}
---
+++ Personal Project/Prototype 3/Assets/Script/PlayerControllerBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Anthony's Jump Code
+public class PlayerControllerBehaviour : MonoBehaviour
+{
+   public float speed = 3f;
+   public float jumpForce = 20f;
+   public int jumpCountMax = 2;
+   public float gravityModifier;
+
+
+   private int jumpCount;
+   private Rigidbody rigidbodyObj;
+   private Vector3 direction;
+   
+   private void Start()
+   {
+      rigidbodyObj = GetComponent<Rigidbody>();
+      Physics.gravity *= gravityModifier;
+   }
+
+   private void OnCollisionEnter(Collision other)
+   {
+      jumpCount = 0;
+   }
+
+   private void Update()
+   {
+      direction.x = speed * Input.GetAxis("Horizontal");
+      rigidbodyObj.AddForce(direction, ForceMode.Force);
+
+      if (Input.GetButtonDown("Jump") && jumpCount < jumpCountMax)
+      {
+         direction.y = jumpForce;
+         rigidbodyObj.AddForce(direction, ForceMode.Impulse);
+         jumpCount++;
+      }
+   }
+}
