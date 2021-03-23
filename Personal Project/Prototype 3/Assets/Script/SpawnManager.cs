b837e40ca7d59dfeeea75242413f266b475bcc34
Ben Hood <10885066@my.uvu.edu>
2021-03-22 20:56:13 -0600

    Updating Files (need to figure out how to have random obstecles come at the character).

--- Personal Project/Prototype 3/Assets/Script/SpawnManager.cs
+++ Personal Project/Prototype 3/Assets/Script/SpawnManager.cs
@@ -2,13 +2,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = System.Random;
 
 public class SpawnManager : MonoBehaviour
 {
 
     public GameObject obstaclePrefab;
-  
-    private Vector3 spawnPos = new Vector3(25, 0, 0);
+
+    private Vector3 spawnPos = new Vector3(40, 0, 0);
     private float startDelay = 2;
     private float repeatRate = 2;
     
