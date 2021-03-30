a3e47d4e85b4401dc36cf1397409e06e1f7d4560
Ben Hood <10885066@my.uvu.edu>
2021-03-30 14:53:16 -0600

    Unity file updated

---
+++ Prototype 4/Prototype 4/Assets/Scripts/SpawnManagerBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SpawnManagerBehaviour : MonoBehaviour
+{
+  public GameObject enemyPrefab;
+  private float spawnRange = 9;
+
+  private void Start()
+  {
+    Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+
+  }
+
+  private Vector3 GenerateSpawnPosition()
+  {
+    //Boundraies of Spawn random
+    float spawnPosX = Random.Range(-spawnRange, spawnRange);
+
+    float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+
+    Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+    return randomPos;
+  }
+}
