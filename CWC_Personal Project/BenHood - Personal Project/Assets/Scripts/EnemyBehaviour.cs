6843a5b5236deec240e8baca9e12050316d46b7f
Ben Hood <10885066@my.uvu.edu>
2021-03-29 23:03:36 -0600

    Update in "My Game" colliders and script

---
+++ CWC_Personal Project/BenHood - Personal Project/Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyBehaviour : MonoBehaviour
+{
+   public Transform destination;
+   public NavMeshAgent agent;
+
+   private void Start()
+   {
+      agent = GetComponent<NavMeshAgent>();
+      destination = transform;
+   }
+
+   private void OnTriggerEnter(Collider other)
+   {
+      destination = other.transform;
+   }
+
+   private void OnTriggerExit(Collider other)
+   {
+      destination = transform;
+   }
+
+   private void Update()
+   {
+      agent.destination = destination.position;
+   }
+}
