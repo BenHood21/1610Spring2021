279eda69fde16a8256edec2a75b88e7a5168f71b
Ben Hood <10885066@my.uvu.edu>
2021-03-31 16:14:25 -0600

    Prefabs and scripts change

---
+++ Prototype 4/Prototype 4/Assets/Scripts/PowerupRotateBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupRotateBehaviour : MonoBehaviour
+{
+
+    public float amplitude;
+    public float frequency;
+
+  //  private Vector3 posOffset = new Vector3();
+  //  private Vector3 temPos = new Vector3();
+    
+    private void Start()
+    {
+     // posOffset = transform.position;
+    }
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f,0.5f,0f));
+
+    // temPos = posOffset;
+      //temPos.y = Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+    //  transform.position = temPos;
+    }
+}
