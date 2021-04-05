7bff1bcd66c87791e9556e6f1194ef388d53cfce
Ben Hood <10885066@my.uvu.edu>
2021-04-05 12:36:11 -0600

    Objects fly into the air and clicking on them destroys them

--- Prototype 5/Prototype 5/Assets/Script/TargetBehaviour.cs
+++ Prototype 5/Prototype 5/Assets/Script/TargetBehaviour.cs
@@ -1,18 +1,55 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class TargetBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody targetRb;
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = 0;
+
     void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+     
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+     
+        transform.position = RandomSpawnPos();
+    }
+
+    private void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
-        
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
     }
 }
