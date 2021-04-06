7d20f8ee2d584da3c566b59dadf3060d7e93d5cb
Ben Hood <10885066@my.uvu.edu>
2021-04-06 17:45:01 -0600

    Title and buttons figure out for lesson "What’s the Difficulty"

---
+++ Prototype 5/Prototype 5/Assets/Script/DifficultyButtonBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButtonBehaviour : MonoBehaviour
+{
+    public int difficulty;
+    
+    private GameManagerBehaviour gameManager;
+    private Button button;
+    
+    void Start()
+    {
+        button = GetComponent<Button>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManagerBehaviour>();
+        
+        button.onClick.AddListener(SetDifficulty);
+    }
+
+    void SetDifficulty()
+    {
+        gameManager.StartGame(difficulty);
+        Debug.Log(button.gameObject.name + "was clicked ");
+    }
+}
