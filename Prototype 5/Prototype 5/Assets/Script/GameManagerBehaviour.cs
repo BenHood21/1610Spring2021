b5aad5ab949bf33c7cb3565e25327f9dbd97270e
Ben Hood <10885066@my.uvu.edu>
2021-04-06 17:44:46 -0600

    Title and buttons figure out for lesson "What’s the Difficulty"

--- Prototype 5/Prototype 5/Assets/Script/GameManagerBehaviour.cs
+++ Prototype 5/Prototype 5/Assets/Script/GameManagerBehaviour.cs
@@ -1,43 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class GameManagerBehaviour : MonoBehaviour
 {
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
+     public Button restartButton;
     public bool isGameActive;
-    public Button restartButton;
+    public GameObject titleScreen;
 
     private float spawnRate = 1f;
     private int score;
     
-    
-    void Start()
-    {
-        isGameActive = true;
-        StartCoroutine(SpawnTarget());
-        score = 0;
-        UpdateScore(0);
-        
-    }
-
-    public void RestartGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
-    
-    
-    public void GameOver()
-    {
-        restartButton.gameObject.SetActive(true);
-        gameOverText.gameObject.SetActive(true);
-        isGameActive = false;
-    }
 
     IEnumerator SpawnTarget()
     {
@@ -46,10 +27,6 @@ public class GameManagerBehaviour : MonoBehaviour
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
-
-           
-
-          
         }
     }
 
@@ -58,4 +35,30 @@ public class GameManagerBehaviour : MonoBehaviour
         score += scoreToAdd;
         scoreText.text = "Score: " + score;
     }
+     
+    public void GameOver()
+    {
+        restartButton.gameObject.SetActive(true);
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+    
+     public void RestartGame()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        score = 0;
+        spawnRate = spawnRate / difficulty;
+        
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+        
+        titleScreen.gameObject.SetActive(false);
+    }
+    
+    
 }
