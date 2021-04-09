dabb0308ffda38ed331d260a41d52e7429041ea6
Ben Hood <10885066@my.uvu.edu>
2021-04-08 23:03:50 -0600

    Fixed character direction, need to figure out why camera wont rotate

--- CWC_Personal Project/BenHood - Personal Project/Assets/Scripts/CameraFollowBehaviour.cs
+++ CWC_Personal Project/BenHood - Personal Project/Assets/Scripts/CameraFollowBehaviour.cs
@@ -6,10 +6,14 @@ using UnityEngine;
 public class CameraFollowBehaviour : MonoBehaviour
 {
     public GameObject player;
+    public float rotationSpeed;
+    
     private Vector3 offset = new Vector3(-2, 9, 0);
 
     private void Update()
     {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
         transform.position = player.transform.position + offset;
     }
 }
