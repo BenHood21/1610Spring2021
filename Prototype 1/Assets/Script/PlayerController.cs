90ee6df9db4eedb9cc303af9c33cab31c1494f73
Ben Hood <10885066@my.uvu.edu>
2021-04-13 12:32:38 -0600

    AddRelativeForce update added

--- Prototype 1/Assets/Script/PlayerController.cs
+++ Prototype 1/Assets/Script/PlayerController.cs
@@ -1,21 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float vehicleSpeed = 10.0f;
-    [SerializeField]  float turnSpeed = 40f;
+    [SerializeField] float horsePower = 0f;
+   private const float turnSpeed = 40f;
     private float horizontalInput;
     private float forwardInput;
-    
+
+    private Rigidbody playerRb;
+
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
     // Forward/reverse movement control and turn control
     void FixedUpdate()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
         
-        transform.Translate(Vector3.forward * Time.deltaTime * vehicleSpeed * forwardInput);
+        playerRb.AddRelativeForce(Vector3.forward * horsePower * forwardInput);
         transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
