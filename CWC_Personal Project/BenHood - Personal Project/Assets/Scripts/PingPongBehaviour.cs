d841425a6e635b87c7dcfce1b7e9aa035a48209a
Ben Hood <10885066@my.uvu.edu>
2021-04-19 23:34:11 -0600

    Added to personal game

---
+++ CWC_Personal Project/BenHood - Personal Project/Assets/Scripts/PingPongBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PingPongBehaviour : MonoBehaviour
+{
+
+    private float min;
+    private float max;
+
+    public float barrierSpeed = 10f, barrierPosition;
+    public float posMin, posMax;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        min = transform.position.x - posMax;
+        max = transform.position.x + posMax;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.PingPong( barrierPosition),
+            (Time.time * barrierSpeed, posMax - posMin) + posMin, transform.position.y);
+
+    }
+}
