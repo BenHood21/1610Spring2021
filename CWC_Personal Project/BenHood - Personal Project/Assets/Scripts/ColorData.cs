c85c9959f323b41f95c6c43b9fcc830ee71683a4
Ben Hood <10885066@my.uvu.edu>
2021-04-28 10:49:00 -0600

    Added ColorData.cs

---
+++ CWC_Personal Project/BenHood - Personal Project/Assets/Scripts/ColorData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorData: MonoBehaviour
+{
+  public Color value;
+
+  public void ChangeImageColor(Image imageObj)
+  {
+      imageObj.color = value;
+  }
+
+  public void ChangeMeshRenderColor(MeshRenderer meshObj)
+  {
+      meshObj.material.color = value;
+  }
+
+  public void ChangeSpriteRenderColor(SpriteRenderer spriteObj)
+  {
+      spriteObj.color = value;
+  }
+}
