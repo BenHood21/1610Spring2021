4a540f44a20d4b1249177ca6a5298adde4b385c4
Ben Hood <10885066@my.uvu.edu>
2021-04-28 18:28:11 -0600

    Unversioned Files

---
+++ CWC_Personal Project/BenHood - Personal Project/Assets/Scripts/TriggerEventBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEventBehaviour : MonoBehaviour
+{
+
+  public UnityEvent triggerEnterEvent;
+
+  private void OnTriggerEnter(Collider other)
+  {
+    triggerEnterEvent.Invoke();
+  }
+}
