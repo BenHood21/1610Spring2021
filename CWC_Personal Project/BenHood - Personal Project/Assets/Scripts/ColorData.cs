0b474c9ea41dd0db7e135670dbc2ab4993858e20
Ben Hood <10885066@my.uvu.edu>
2021-04-28 18:27:42 -0600

    Key and Door UI fixed

--- CWC_Personal Project/BenHood - Personal Project/Assets/Scripts/ColorData.cs
+++ CWC_Personal Project/BenHood - Personal Project/Assets/Scripts/ColorData.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ColorData: MonoBehaviour
+[CreateAssetMenu]
+public class ColorData: ScriptableObject
 {
   public Color value;
 
